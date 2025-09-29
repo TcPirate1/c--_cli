e1db67f97c433b504e4efcda5786d15c0ab2c07f
TC_Linux <88120195+TcPirate1@users.noreply.github.com>
2025-09-30 00:41:30 +1300

    Add checkers. Created a functions for things I think I'll reuse.

--- ffdb.cpp
+++ ffdb.cpp
@@ -1,12 +1,23 @@
+#include <cstddef>
 #include <cstdlib>
+#include <cstring>
 #include <fstream>
 #include <getopt.h>
 #include <iostream>
 #include <vector>
+#include <filesystem>
 
-int main(int argc, char *argv[]) {
-  int option;
-  struct option long_options[] = {
+void validChar(char enteredChar []) {
+  int length = strlen(enteredChar);
+  while (true) {
+    // if () {
+    //   std::cout << enteredChar;
+    //   break;
+    // }
+  }
+}
+
+struct option long_options[] = {
       {"help", no_argument, nullptr, 'h'},
       {"version", no_argument, nullptr, 'v'},
       {"create", no_argument, nullptr, 'c'},
@@ -14,48 +25,68 @@ int main(int argc, char *argv[]) {
       {"update", no_argument, nullptr, 'u'},
       {"delete", no_argument, nullptr, 'd'},
   };
-  std::vector<int> collections;
 
-  if (argc > 4) {
-    std::cout << "Too many options";
-  }
-  if (argc == 1) {
-    std::cout << "List collections:\n";
-  } else {
-    while ((option = getopt_long(argc, argv, "hvcaud", long_options,
-                                 nullptr)) != -1) {
-      switch (option) {
-      case 'h':
-        std::cout << "FF cli database\n"
-                  << "Usage: FFdb [options]\n"
+void helpStdOut() {
+  std::cout << "Usage: FF tracker [options]\n"
                   << "--help OR -h           Show man page\n"
                   << "--version OR -v        CLI version\n"
                   << "--create or -c NAME    Create collection\n"
                   << "--add or -a            Add to collection\n"
                   << "--update or -u         Update collection\n"
                   << "--delete or -d         Delete from collection\n";
+}
+
+void dirChecker(std::string fPath) {
+  if (!std::filesystem::exists(fPath) && !std::filesystem::is_directory(fPath)) {
+          std::filesystem::create_directory(fPath);
+        }
+}
+
+int main(int argc, char *argv[]) {
+  std::cout << "FF cli tracker\n";
+
+  int option;
+  
+  std::vector<std::string> collections;
+  // Possible can just use std::cin::fail() -> returns bool, to check whether it's a char, therefore don't need
+  // to check length, just need to check input contains valid chars.
+
+  if (argc == 1) {
+    std::cout << collections.size();
+    helpStdOut();
+} else {
+    while ((option = getopt_long(argc, argv, "hvcaud", long_options, nullptr)) != -1) {
+      switch (option) {
+
+      case 'h':
+        helpStdOut();
         return 0;
+
       case 'v':
         std::cout << "Version: 0.0.1\n";
         return 0;
-      // Start by checking if there are any collections. IF there aren't any,
-      // ask to create one. If there are collections, ask which one they'd like
-      // to choose.
+      
       case 'c': {
         // getenv with "HOME" as the parameter will expand to "home/user/"
-        std::string home = getenv("HOME");
+        const char* home = getenv("HOME");
+        std::string homedir = home;
+        if (home == NULL) {
+          std::cout << "This is null";
+        }
         std::string collectionName;
         std::cout << "What would you like to name your collection?\n";
-        getline(std::cin, collectionName);
+        std::getline(std::cin, collectionName);
         std::cout << "Making " << collectionName << "...";
-        std::string finalPath = home + "/Documents/" + collectionName + ".txt";
-        // There should be some way to tell user path is not found
-        // open does not create to specified directory, use something else??
+        std::string fPath = homedir + "/Documents/FF/";
+        dirChecker(fPath);
+        std::string finalPath = fPath + collectionName + ".txt";
         std::ofstream outfile(finalPath);
         std::cout << "Finished making " << finalPath;
+        collections.push_back(collectionName);
         outfile.close();
       }
         return 0;
+
       case 'a':
         std::cout << "Enter the data you'd like to add to collection:\n";
         break;
