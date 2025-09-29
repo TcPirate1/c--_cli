2a161da46d85a9015a91e7e018b0d97f9a2e4a8d
TC_Linux <88120195+TcPirate1@users.noreply.github.com>
2025-09-29 13:43:53 +1300

    Stole some functionality from another bash script. Added g++ to compiling options.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-clang++ -o2 -g ffdb.cpp -o ffdb
+isValidCharacter()
+{
+	local prompt="$1"
+	local valid_characters="$2"
+
+	while true; do
+		read -r -p "$prompt" choice
+		choice="${choice,,}"
+		if [[ "$valid_characters" == *"$choice"* && ${#choice} == 1 ]]; then
+			# Check if choice matches valid_characters and is 1 character long
+			printf %s "$choice"
+			break
+		fi
+	done
+}
+
+input=$(isValidCharacter "[c]lang++ or [g]cc?
+" "cg")
+
+if [ "$input" == "c" ]; then
+clang++ -o2 ffdb.cpp -o ffdb_clang # Some reason HAS permissions
+printf "Your C++ code has been compiled"
+elif [ "$input" == "g" ]; then
+g++ -S ffdb.cpp -o ffdb_g++ # Some reason doesn't have permissions
+printf "Your C++ code has been compiled"
+fi
 
 # Remove -g if debugger not needed
 # Will need to create an interactive script.
